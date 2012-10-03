879e3cdb335a8152f1df30f4264d8e8f5d5654a1
arturovolpe@gmail.com <arturovolpe@gmail.com@51f13cf7-c51e-c26e-d34e-6f520c80cb65>
2012-10-03 01:17:51 +0000

    Creacion de la vista de VentaABM

---
+++ trunk/webstock/WebStockGWT/src/py/com/pg/webstock/gwt/server/service/DetalleVentaServiceImpl.java
@@ -0,0 +1,48 @@
+package py.com.pg.webstock.gwt.server.service;
+
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
+import py.com.pg.webstock.entities.DetalleVenta;
+import py.com.pg.webstock.entities.Producto;
+import py.com.pg.webstock.entities.Venta;
+import py.com.pg.webstock.gwt.client.service.DetalleVentaService;
+
+public class DetalleVentaServiceImpl extends BaseDAOServiceImpl<DetalleVenta>
+		implements DetalleVentaService {
+
+	@Override
+	public DetalleVenta add(DetalleVenta entidad) {
+		try {
+			ut.begin();
+			em.merge(entidad);
+			Producto p = em.find(Producto.class, entidad.getProducto().getId());
+			System.out.println("AAAAAAAAAAA" + entidad.getCantidad());
+			p.setCantidad(p.getCantidad() - entidad.getCantidad());
+			em.merge(p);
+			ut.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return entidad;
+	}
+	public List<DetalleVenta> getByVenta(Venta venta) {
+		Venta entidad = em.find(Venta.class, venta.getId());
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+
+		CriteriaQuery<DetalleVenta> q = cb.createQuery(DetalleVenta.class);
+		Root<DetalleVenta> root = q.from(DetalleVenta.class);
+		ParameterExpression<Venta> parameterVenta = cb.parameter(Venta.class);
+		q.select(root).where(cb.equal(root.get("venta"), parameterVenta));
+		TypedQuery<DetalleVenta> query = em.createQuery(q);
+		query.setParameter(parameterVenta, entidad);
+		return filter(query.getResultList());
+	}
+
+	private static final long serialVersionUID = -5691759970760339697L;
+}
---
+++ trunk/webstock/WebStockGWT/src/py/com/pg/webstock/gwt/server/service/VentaServiceImpl.java
@@ -0,0 +1,10 @@
+package py.com.pg.webstock.gwt.server.service;
+
+import py.com.pg.webstock.entities.Venta;
+import py.com.pg.webstock.gwt.client.service.VentaService;
+
+public class VentaServiceImpl extends BaseDAOServiceImpl<Venta> implements
+		VentaService {
+
+	private static final long serialVersionUID = -5691759970760339697L;
+}
